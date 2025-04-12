2f97b29dbc0f52366faf00d83cab00b7a641fd0d
wellingtonfoz <wellington.oliveira.foz@gmai.com>
2025-04-12 09:56:57 -0300

    Implementação do método getById

--- src/models/enderecoModel.js
+++ src/models/enderecoModel.js
@@ -1,8 +1,18 @@
 const enderecos = require('../data/enderecos.json');
 
 class EnderecoModel {
+  
   static getAll() {
     return enderecos;
+  }
+
+  static getById(id) {
+    for(let i=0; i<enderecos.length; i++){
+      if(enderecos[i].id == id)
+        return enderecos[i];
+    }
+
+    return 404;
   }
 
 
